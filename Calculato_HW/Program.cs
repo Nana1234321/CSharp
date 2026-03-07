b8898fe6c464a9e5fe96370a2c70416667c5efc3
paramonova.as <paramonova.as@phystech.edu>
2026-03-08 00:14:56 +0300

    Add project files.

---
+++ Calculato_HW/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("CALCULATOR( exit = press q)");
+        while (true)
+        {
+            try
+            {
+                Console.Write("\nFirst number");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "q") break;
+                double a = Convert.ToDouble(input);
+
+                Console.Write("Second number");
+                input = Console.ReadLine();
+                if (input.ToLower() == "q") break;
+                double b = Convert.ToDouble(input);
+
+                Console.Write("Operation");
+                string op = Console.ReadLine();
+                if (input.ToLower() == "q") break;
+
+                double res = 0;
+                switch (op)
+                {
+                    case "+": res = a + b; break;
+                    case "-": res = a - b; break;
+                    case "*": res = a * b; break;
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Error1");
+                        }
+                        res = a / b; break;
+                    default:
+                        Console.WriteLine(" Error2");
+                        continue;
+                }
+                Console.WriteLine($"Result: {a} {op} {b} = {res}");
+
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error");
+            }
+        }
+        Console.WriteLine("Program done");
+
+    }
+}
